7197438b7b9b852f5cf7bfcbe253f33f625bb667
Hamza Yahamdi <hamza.yahamdi93@gmail.com>
2025-01-27 13:17:06 +0100

    test2

--- lib/env.ts
+++ lib/env.ts
@@ -1,3 +1,3 @@
 export const env = {
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY || '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'
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY
 } 
